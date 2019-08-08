07e2ef576601e62665c946e13f4ced6b1b3437bf
wwwwtao <934725827@qq.com>
2019-08-08 15:46:44 +0800

    学习

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+
+import {AxiosRequestConfig} from "./types"
+
+function axios(config: AxiosRequestConfig) {
+    //
+}
+
+export default axios
---
+++ src/types/index.ts
@@ -0,0 +1,14 @@
+export type Method = 'get' | 'GET'
+| 'post' | 'POST'
+| 'delete' | 'Delete' //删除
+| 'head' | 'HEAD' //头
+| 'options' | 'OPTIONS' //选项
+| 'put' | 'PUT' //放
+| 'patch' | 'PATCH' //补丁
+
+export interface AxiosRequestConfig {
+    url: string
+    method?: Method
+    data?: any
+    params?: any
+}
